61431233c73ae2489e8b9c096ab16c97e4b5e626
p22rlugo <p22rlugo@fairfieldprep.org>
2022-04-26 09:38:46 -0400

    add files

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,8 +1,5 @@
-
-
 # Imports
 from string import ascii_uppercase
-
 
 # Functions
 def cipher_key(shift):
@@ -11,7 +8,6 @@ def cipher_key(shift):
     shifted_letters = ascii_uppercase[int(shift):] + ascii_uppercase[:int(shift)]
 
     return dict(zip(original_letters, shifted_letters))
-
 
 def shift_line(line, dict_key):
     new_line = ""
@@ -30,20 +26,23 @@ def shift_line(line, dict_key):
         # Return "fixed" line
         new_line = new_line + dict_key[letter]
     
+    # Return has to return a list because it can't return one giant string???
     return new_line
 
 
 def encrypt_message(filename, dict_key):
+    fixed = []
+    final = ""
     with open(filename) as file:
         for line in file:
-            fixed = shift_line(line,dict_key)
-    file = open("encrypted_test.txt","w")
-    file.write(fixed)
-    file.close()
+            fixed += shift_line(line,dict_key)
+        for lines in fixed:
+            final = final + lines
+        file = open("encrypted_test.txt","w")
+        file.write(final)
+        file.close()
 
     
-
-
 # Main
 user_file = input("Please enter a file to be encrypted: ")
 shift_value = input("Please enter a shift value: ")
